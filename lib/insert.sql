2c04a43dbe08410e65e922811c154c38505ce13a
kshayes78 <89166628+kshayes78@users.noreply.github.com>
2022-02-27 10:26:49 -0500

    commit tree 23ec8b74caa0eb2a0f36cceed1ed91ab5b6ea67f

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 10, 'M', 'brown', 'friendly', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 4, 'M', 'black', 'mean', FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 6, 'F', 'black', 'friendly', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 12, 'M', 'brown', 'timid', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Melissa', 8, 'F', 'brown', 'friendly', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 15, 'M', 'black', 'mean', FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 1, 'F', 'brown', 'friendly', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (null, 2, 'M', 'brown', 'timid', TRUE);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,14 +1,14 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  SELECT name, age FROM bears WHERE sex="F"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  SELECT bears.name FROM bears ORDER BY name ASC
 end
 
-def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
-end
+# def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
+#   # SELECT name WHERE alive = TRUE and ORDER by age DESC
+# end
 
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
